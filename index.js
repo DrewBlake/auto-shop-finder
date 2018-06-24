f7fb4ac47ce9e511b3b5f96ae7b9ce40ba283322
Andrew Blake <drewblake1974@gmail.com>
2018-06-23 20:38:31 -0400

    created form input and some css

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
+// Initialize and add the map
+/*function initMap() {
+  // The location of Uluru
+  var uluru = {lat: 33.6220542, lng: -84.36909179999999};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}*/
+
+const GOOGLE_PLACES_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
+
+function getDataFromApi(callback) {
+  const query = {
+    location: '33.62,-84.36',
+    rankby: 'distance',
+    type: 'car_repair',
+    key: 'AIzaSyDXM1Agyu1TixKQJWNTzvwf-ITCGijsuBk'
+    
+  }
+  $.getJSON(GOOGLE_PLACES_URL, query, callback);
+}
+
+
+
+function renderResult(result) {
+  return `<p>${result.name}</p>`;
+}
+
+function displayNearByData(data) {
+  const results = data.results.map((item, index) => renderResult(item));
+  $('.js-show-result').html(results);
+  console.log('hello1');
+
+}
+
+function renderForm() {
+  return `
+      <form action="#" class="js-search-form">
+        <label for="query"><h2>Enter a city or zip code</h2></label>
+        <input type="text" id="query" class="js-query" placeholder="Example: Atlanta or 30297" required>
+        <button type="submit">Search</button>
+      </form>
+      <div id="userInput"></div>`;
+}
+
+function displayForm() {
+  $("#userForm").html(renderForm);
+}
+
+ function handleSearchButtonClick() {
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    console.log(query);
+    // clear out the input
+    $("#userInput").html(`<p>The location you entered is ${query}</p>`);
+    queryTarget.val("");
+ });
+}
+
 function getLocation() {
     
         /*navigator.geolocation.getCurrentPosition(function(position){
@@ -6,45 +69,63 @@ function getLocation() {
         });*/
         if (navigator.geolocation) {
           console.log('geolocation');
-        }
-        console.log('hello');
-        let x = navigator.geolocation;
-        console.log(x);
         
-        x.getCurrentPosition(success, failure);
+          console.log('hello');
+          let x = navigator.geolocation;
+          console.log(x);
+          
+          x.getCurrentPosition(success, failure);
 
-        function success(position) {
-          let myLat = position.coords.latitude;
-          let myLong = position.coords.longitude;
-          $("#lat").html(myLat);
-          $("#long").html(myLong);
-          console.log('goodjob');
-          $("#failure").html('<p>worked</p>') 
+          function success(position) {
+            let myLat = position.coords.latitude;
+            let myLong = position.coords.longitude;
+            $("#lat").html(`<p>Your Latitude is ${myLat}</p>`);
+            $("#long").html(`<p>Your Longitude is ${myLong}</p>`);
+            console.log('goodjob');
+            
+          }
+
+          function failure() {
+            
+            $("#failure").html('<p>Cannot show your location unless you choose allow</p>');
+            
+          }
+        } else {
+          
+          $("#failure").html('<p>Geolocation is not supported by Your browser</p>');
         }
         
-        function failure() {
-          $("#failure").html('<p>Failed</p>')
-        }
-        
-        $(".js-show-result").html('<p>Im here</p>');
-    
 }
 /*function showPosition(position) {
     $('.js-show-result').html(`<p>Latitude:
     <br>Longitude: </p>`);
     //console.log(`${position.coords.latitude}`); 
 }
+*/
+
+function handleAnywhereButtonClick() {
+  $('.js-location-anywhere').on('click', function(event) {
+    //$("#lat").remove();
+    //$("#long").remove();
+    //$("failure").remove();
+    displayForm();
+    handleSearchButtonClick();
+  });
+}
 
 function handleLocationButtonClick() {
-  $('.js-location').on('click', function(event) {
+  $('.js-location-near-me').on('click', function(event) {
+    //$("#long").remove();
     getLocation();
+    //getDataFromApi(displayNearByData);
   });
-}*/
+}
 
 function runApp() {
   //success();
-  getLocation();
-  
+  //initMap();
+  handleLocationButtonClick();
+  handleAnywhereButtonClick();
   //showPosition();
   //handleLocationButtonClick();
 }
